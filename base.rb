60fb7c97b3f8fdb156e2f379d3f864c80c3c9a45
John Parker <jparker@urgetopunt.com>
2009-12-14 03:55:02 -0800

    Cleanup, new dependencies

--- base.rb
+++ base.rb
@@ -1,35 +1,57 @@
 # $ rails <application name> -m http://github.com/jparker/rails-templates/...
-#
-# load_template 'http://github.com/jparker/rails-templates/...'
-# generate :generator_name, args
 
 run "echo TODO > README"
 
-gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com'
-gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
-gem 'webrat'
+gem 'faker'
+gem 'machinist'
+file 'spec/blueprints.rb', <<-END
+require 'machinist/active_record'
+require 'sham'
+Dir[File.expand_path(File.join(File.dirname(__FILE__), 'blueprints', '**', '*.rb'))].each {|f| require f}
+END
+run 'mkdir -p spec/blueprints'
+run 'touch spec/blueprints/.gitignore'
 gem 'rr'
-gem 'redgreen'
+gem 'shoulda'
+gem 'webrat'
+gem 'cucumber'
 
-gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+generate 'rspec'
+generate 'cucumber'
 
-gem 'haml'
-run 'haml --rails .'
+spec_helper_contents = File.read('spec/spec_helper.rb')
+spec_helper_contents.sub!(/^(Spec::Runner\.configure)/, "require 'blueprints'\n\n\\1")
+spec_helper_contents.sub!(/# (config\.mock_with :rr)/, "\\1\n")
+file 'spec/spec_helper.rb', spec_helper_contents
 
 gem 'newrelic_rpm'
-puts "Don't forget to install your newrelic config in config/newrelic.yml!"
+puts <<-END
 
+  ******************
+  ***
+  *** Install config from http://rpm.newrelic.com in config/newrelic.yml
+  ***
+  ******************
+
+END
 hoptoad_api_key = ask("What is your Hoptoad API key (leave blank to skip)?")
-unless hoptoad_api_key.blank?
+if hoptoad_api_key.present?
   plugin 'hoptoad_notifier', :git => 'git://github.com/thoughtbot/hoptoad_notifier.git'
-  initializer 'hoptoad.rb', <<-CODE
+  initializer 'hoptoad.rb', <<-END
 HoptoadNotifier.configure do |config|
   config.api_key = '#{hoptoad_api_key}'
 end
-  CODE
+  END
 end
 
-git :init
+gem 'will_paginate'
+gem 'formtastic'
+gem 'haml'
+run 'haml --rails .'
+gem 'authlogic'
+
+plugin 'validation_reflection', :git => 'git://github.com/redinger/validation_reflection.git'
+plugin 'urgetopunt_helpers', :git => 'git://github.com/jparker/urgetopunt_helpers.git'
 
 file '.gitignore', <<-END
 .DS_Store
@@ -47,19 +69,15 @@ run 'mkdir -p public/stylesheets/sass'
 run 'touch public/stylesheets/sass/.gitignore'
 run 'cp config/database.yml config/example_database.yml'
 
-# TODO: Better to edit existing version rather than overwrite?
-file 'test/test_helper.rb', <<-CODE
-ENV['RAILS_ENV'] = 'test'
-require File.expand_path(File.dirname(__FILE__) + '/../config/environment')
-require 'test_help'
-require 'redgreen' unless ENV['TM_MODE']
+run 'mkdir -p app/mailers app/observers spec/mailers spec/observers'
 
-class ActiveSupport::TestCase
-  self.use_transactional_fixtures = true
-  self.use_instantiated_fixtures = false
-  
-  include RR::Adapters::TestUnit
-end
-CODE
+extra_load_paths = %w[mailers observers].map { |path| "\#{RAILS_ROOT}/app/#{path}" }.join(' ')
+environment_contents = File.read('config/environment.rb')
+environment_contents.sub!(/# (config\.load_paths \+=) .*/, "\\1 %W( #{extra_load_paths} )")
+file 'config/environment.rb', environment_contents
 
-git :add => '.', :commit => '-m "Initial import"'
+run 'curl -L http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js > public/javascripts/jquery.js'
+
+git :init
+git :add => '.'
+git :commit => '-m "Initial import"'
