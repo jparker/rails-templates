943b2445326fcfd970a0e100dd833525aa2e5ed5
John Parker <jparker@urgetopunt.com>
2011-01-12 11:54:20 -0800

    new rails3 version of template

--- base.rb
+++ base.rb
@@ -1,136 +1,88 @@
-# $ rails <application name> -m http://github.com/jparker/rails-templates/...
+gem 'rspec-rails',    :group => [:test, :development], :version => '~> 2.3'
+gem 'rspec',          :group => :test, :version => '~> 2.3'
+gem 'cucumber',       :group => :test
+gem 'cucumber-rails', :group => :test
+gem 'shoulda',        :group => :test
+gem 'faker',          :group => [:test, :development]
 
-run "echo TODO > README"
+# Capybara 0.4 bugging out when following links (undefined method node...)
+gem 'capybara', :version => '~> 0.3.9', :group => :test
+# gem 'webrat', :version => '0.7.2.beta.1', :group => :test
 
-gem 'rspec', :lib => false, :env => 'test'
-gem 'rspec-rails', :lib => false, :env => 'test'
-gem 'cucumber', :env => 'test'
-gem 'cucumber-rails', :env => 'test'
-gem 'machinist', :env => 'test'
-gem 'shoulda', :env => 'test'
-gem 'mocha', :env => 'test'
-gem 'webrat', :env => 'test'
-gem 'pickle', :env => 'test'
-gem 'faker', :env => 'test'
+gem 'will_paginate',       :version => '>= 3.0.pre2'
+gem 'formtastic',          :version => '~> 1.2.2'
+gem 'inherited_resources', :version => '~> 1.1.2'
+gem 'haml',                :version => '~> 3.0.25'
+gem 'devise',              :version => '~> 1.1.5'
+gem 'hoptoad_notifier',    :version => '~> 2.3.12'
 
-generate 'rspec'
-generate 'cucumber'
-generate 'pickle'
+gem 'rails3-generators', :group => :development
+gem 'haml-rails',        :group => :development
+gem 'jquery-rails',      :group => :development
 
-contents = File.read('spec/spec_helper.rb')
-contents.sub!(/^(Spec::Runner\.configure)/, "require 'blueprints'\nrequire 'authlogic/test_case'\n\n\\1")
-contents.sub!(/# (config\.mock_with :mocha)/, "\\1\n")
-contents.sub!(/^end/, "\n  config.before(:all) { Sham.reset(:before_all) }\n  config.before(:each) { Sham.reset(:before_each) }\nend")
-file 'spec/spec_helper.rb', contents
+generate 'rspec:install'
+generate 'cucumber:install', '--capybara'
+generate 'jquery:install'
+generate 'formtastic:install'
+generate 'devise:install'
 
-file 'spec/blueprints.rb', <<-END
-require 'machinist/active_record'
-require 'sham'
-require 'faker'
-Dir[File.expand_path(File.join(File.dirname(__FILE__), 'blueprints', '**', '*.rb'))].each {|f| require f}
-END
-run 'mkdir -p spec/blueprints'
-run 'touch spec/blueprints/.gitignore'
+# gem 'machinist', :version => '>= 2.0.0.beta2', :group => :test
+# generate 'machinist:install'
+gem 'factory_girl_rails', :group => :test
 
-if File.exists?('features/support/env.rb')
-  contents = File.read('features/support/env.rb')
-else
-  contents = ''
-end
-contents << <<-END
-require "\#{Rails.root}/spec/blueprints"
-Before { Sham.reset }
-END
-file 'features/support/env.rb', contents
-
-# gem 'newrelic_rpm'
-# puts <<-END
-# 
-#   ******************
-#   ***
-#   *** Install config from http://rpm.newrelic.com in config/newrelic.yml
-#   ***
-#   ******************
-# 
-# END
-
-hoptoad_api_key = ask("What is your Hoptoad API key (leave blank to skip)?")
+hoptoad_api_key = ask('What is the Hoptoad API key for this project (leave blank to skip)?')
 if hoptoad_api_key.present?
-  gem 'hoptoad_notifier'
-  generate 'hoptoad', :api_key => hoptoad_api_key
-end
-
-gem 'will_paginate'
-gem 'formtastic'
-gem 'haml'
-run 'haml --rails .'
-gem 'authlogic'
-
-rake 'gems:unpack'
-rake 'gems:unpack', :env => 'test'
-
-initializer 'sass.rb', <<-END
-if Rails.env.development?
-  Sass::Plugin.options[:always_update] = true
-  Sass::Plugin.options[:line_comments] = true
-  Sass::Plugin.options[:style] = :expanded
+  generate 'hoptoad', '--api-key', hoptoad_api_key
+  gsub_file 'config/initializers/hoptoad.rb', 'end', "  config.js_notifier = true\nend"
 else
-  Sass::Plugin.options[:style] = :compressed
+  puts '>>> Skipping hoptoad configuration for now'
+  puts '>>> To configure hoptoad run: rails g hoptoad --api-key HOPTOAD_API_KEY'
+  file 'TODO', "To install hoptoad:\n$ rails g hoptoad --api-key HOPTOAD_API_KEY\n"
+end
+
+gsub_file 'spec/spec_helper.rb', 'config.mock_with :rspec', '# config.mock_with :rspec'
+# gem 'rr', :group => :test
+# gsub_file 'spec/spec_helper.rb', '# config.mock_with :rr', 'config.mock_with :rr'
+gem 'mocha', :group => :test
+gsub_file 'spec/spec_helper.rb', '# config.mock_with :mocha', 'config.mock_with :mocha'
+
+remove_file 'app/views/layouts/application.html.erb'
+file 'app/views/layouts/application.html.haml', <<END
+!!! XML
+!!! 5
+%head
+  %title (untitled)
+  = stylesheet_link_tag :all
+  = csrf_meta_tag
+%body
+  #flash
+    %p.notice= notice
+    %p.error= error
+
+  = yield
+
+  = javascript_include_tag :defaults
+END
+
+# TODO: move to urgetopunt_rails_helper gem
+lib 'migration_helper.rb', <<END
+module MigrationHelper
+  # add_foreign_key_constraint :items, :seller_id             # items.seller_id => sellers.id
+  # add_foregin_key_constraint :messages, :sender_id, :people # messages.sender_id => people.id
+  def add_foreign_key_constraint(table, column, reference_table = nil)
+    reference_table ||= column.to_s.sub(/_id$/, '').tableize
+    execute <<-END.squish
+      ALTER TABLE      \#{connection.quote_table_name table}
+      ADD FOREIGN KEY (\#{connection.quote_column_name column})
+      REFERENCES       \#{connection.quote_table_name reference_table}
+    END
+  end
 end
 END
-
-run 'mkdir -p spec/support/shoulda_macros'
-run 'cp vendor/gems/authlogic-*/shoulda_macros/authlogic.rb spec/support/shoulda_macros'
-
-plugin 'validation_reflection', :git => 'git://github.com/redinger/validation_reflection.git'
-plugin 'urgetopunt_helpers', :git => 'git://github.com/jparker/urgetopunt_helpers.git'
-
-generate 'formtastic'
-
-file 'app/views/layouts/application.html.haml', <<-END
-!!! XML
-!!! Strict
-%html{html_attrs}
-  %head
-    %title= yield(:title) || "Untitled"
-    %meta{"http-equiv" => "Content-Type", :content => "text/html;charset=utf8"}
-    = stylesheet_link_tag 'formtastic', 'formtastic_changes', 'application'
-  %body
-    .container
-      #head
-        %h1= yield(:title)
-      #body
-        - flash.each do |name, msg|
-          = content_tag :div, msg, :id => 'flash', :class => name
-        = yield
-      #foot
+initializer 'extensions.rb', <<END
+require 'migration_helper'
+ActiveRecord::Migration.extend MigrationHelper
 END
 
-file '.gitignore', <<-END
-.DS_Store
-*~
-.#*
-.*.swp
-db/*.sqlite3*
-log/*.log
-tmp/**/*
-config/database.yml
-public/stylesheets/application.css
-END
-run 'touch tmp/.gitignore log/.gitignore'
-run 'mkdir -p public/stylesheets/sass'
-run 'touch public/stylesheets/sass/.gitignore'
-run 'cp config/database.yml config/example_database.yml'
-
-run 'mkdir -p app/mailers app/observers spec/mailers spec/observers'
-
-extra_load_paths = %w[mailers observers].map { |path| "\#{RAILS_ROOT}/app/#{path}" }.join(' ')
-contents = File.read('config/environment.rb')
-contents.sub!(/# (config\.load_paths \+=) .*/, "\\1 %W( #{extra_load_paths} )")
-file 'config/environment.rb', contents
-
-run 'curl -L http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js > public/javascripts/jquery.js'
-
-# git :init
-# git :add => '.'
-# git :commit => '-m "Initial import"'
+# TODO:
+# gem 'newrelic_rpm'
