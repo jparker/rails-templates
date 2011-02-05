833aaf17465df3e8ed1a8c7cd94aa9942eedc31f
John Parker <jparker@urgetopunt.com>
2011-02-04 16:27:00 -0800

    updated gem dependencies

--- gems.rb
+++ gems.rb
@@ -3,12 +3,12 @@ gem 'rspec',                :version => '~> 2.4',           :group => :test
 gem 'cucumber-rails',       :version => '~> 0.4.0.beta.1',  :group => [:test, :development]
 gem 'cucumber',             :version => '~> 0.10.0',        :group => :test
 gem 'capybara',             :version => '~> 0.4.1',         :group => :test
-gem 'database_cleaner',                                     :group => :test
+gem 'database_cleaner',     :version => '~> 0.6.2',         :group => :test
 gem 'factory_girl_rails',   :version => '~> 1.0.1',         :group => [:test, :development]
 gem 'factory_girl',         :version => '~> 1.3.3',         :group => [:test, :development]
 gem 'faker',                                                :group => [:test, :development]
 gem 'shoulda',              :version => '~> 2.11.3',        :group => :test
-gem 'mocha'                 :version => '~> 0.9.10',        :group => :test
+gem 'mocha'                 :version => '~> 0.9.11',        :group => :test
 # required for view testing in rspec -- capybara won't work
 gem 'webrat',               :version => '~> 0.7.3',         :group => :test
 gem 'fuubar',                                               :group => :test
@@ -22,7 +22,7 @@ gem 'inherited_resources',  :version => '~> 1.1.2'
 gem 'haml',                 :version => '~> 3.0.25'
 gem 'devise',               :version => '>= 1.2.rc'
 gem 'cancan',               :version => '~> 1.5.0'
-gem 'hoptoad_notifier',     :version => '~> 2.4.2'
+gem 'hoptoad_notifier',     :version => '~> 2.4.5'
 gem 'escape_utils',         :version => '~> 0.1.9'
 gem 'newrelic_rpm',         :version => '~> 2.13.4'
 
