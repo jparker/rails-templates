3c555e034c1d5a91f90741bacdc3310ec57b4361
John Parker <jparker@urgetopunt.com>
2011-06-10 11:55:30 -0700

    substantial refactoring plus generic authlogic config

--- gems.rb
+++ gems.rb
@@ -1,31 +1,27 @@
-gem 'rspec-rails',          :version => '~> 2.5.0',         :group => [:test, :development]
-gem 'rspec',                :version => '~> 2.5.0',         :group => :test
-gem 'capybara',             :version => '~> 0.4.1',         :group => :test
-gem 'database_cleaner',     :version => '~> 0.6.2',         :group => :test
-gem 'factory_girl_rails',   :version => '~> 1.0.1',         :group => [:test, :development]
-gem 'factory_girl',         :version => '~> 1.3.3',         :group => [:test, :development]
-gem 'faker',                                                :group => [:test, :development]
-gem 'shoulda',              :version => '~> 2.11.3',        :group => :test
-gem 'mocha',                :version => '~> 0.9.11',        :group => :test
-# required for view testing in rspec -- capybara won't work
-gem 'webrat',               :version => '~> 0.7.3',         :group => :test
-gem 'fuubar',                                               :group => :test
-gem 'launchy',              :version => '~> 0.3.7',         :group => :test
+gem 'rspec-rails', :version => '~> 2.6.0', :group => [:test, :development]
+gem 'factory_girl_rails', :version => '~> 1.0.1', :group => [:test, :development]
+gem 'faker', :group => [:test, :development]
+gem 'shoulda', :version => '~> 2.11.3', :group => :test
+gem 'mocha', :version => '~> 0.9.11', :group => :test
+# TODO: switch to version after capybara 1.0 is released
+gem 'capybara', :group => :test, :git => 'https://github.com/jnicklas/capybara'
+gem 'launchy', :version => '~> 0.3.7', :group => :test
 
-# http://groups.google.com/group/mail-ruby/browse_thread/thread/e93bbd05706478dd?pli=1
-gem 'mail',                 :version => '~> 2.2.15'
+gem 'kaminari', :version => '~> 0.12.3'
+gem 'formtastic', :version => '~> 1.2.3'
+gem 'inherited_resources', :version => '~> 1.2.1'
+gem 'haml', :version => '~> 3.1.1'
+gem 'sass', :version => '~> 3.1.1'
+gem 'authlogic', :version => '~> 3.0.2' if @use_authlogic
+gem 'cancan', :version => '~> 1.6.3'
+gem 'hoptoad_notifier', :version => '~> 2.4.9'
+gem 'escape_utils', :version => '~> 0.2.3'
+gem 'newrelic_rpm', :version => '~> 2.14.1'
 
-gem 'kaminari',             :version => '~> 0.12.3'
-gem 'formtastic',           :version => '~> 1.2.3'
-gem 'inherited_resources',  :version => '~> 1.2.1'
-gem 'haml',                 :version => '~> 3.1.1'
-gem 'sass',                 :version => '~> 3.1.1'
-gem 'devise',               :version => '~> 1.3.3'
-gem 'cancan',               :version => '~> 1.6.3'
-gem 'hoptoad_notifier',     :version => '~> 2.4.9'
-gem 'escape_utils',         :version => '~> 0.2.3'
-gem 'newrelic_rpm',         :version => '~> 2.14.1'
+gem 'rails3-generators', :group => :development
+gem 'haml-rails', :group => :development
+gem 'jquery-rails', :version => '~> 1.0', :group => :development
 
-gem 'rails3-generators',                                    :group => :development
-gem 'haml-rails',                                           :group => :development
-gem 'jquery-rails',         :version => '~> 1.0',           :group => :development
+gem 'rack-ssl', :version => '~>1.3.2', :group => :production if @require_ssl
+
+run 'bundle install'
--- main.rb
+++ main.rb
@@ -1,35 +1,91 @@
-class Rails::Generators::AppGenerator
-  def todo(component, message)
-    say "\033[36m" + 'todo'.rjust(10) + "\033[0m    check TODO file when configuring #{component}"
-    create_file 'TODO', '' unless File.exist?('TODO')
-    append_file 'TODO', "---\n#{message}\n"
-  end
+def todo(component, message)
+  say "\033[36m" + 'todo'.rjust(10) + "\033[0m    check TODO file when configuring #{component}"
+  create_file 'TODO', '' unless File.exist?('TODO')
+  append_file 'TODO', "---\n#{message}\n"
 end
 
+@require_ssl = yes?('Will this application require SSL in production?')
+@use_authlogic = yes?('Will this application use Authlogic for authentication?')
+@hoptoad_api_key = ask("What is this app's Hoptoad API key (leave blank to skip)?")
+
 apply File.join(File.dirname(__FILE__), 'gems.rb')
-apply File.join(File.dirname(__FILE__), 'defaults.rb')
-apply File.join(File.dirname(__FILE__), 'urgetopunt.rb')
 
 generate 'rspec:install'
-append_file '.rspec', "--format Fuubar\n"
 gsub_file 'spec/spec_helper.rb', 'config.mock_with :rspec', '# config.mock_with :rspec'
 gsub_file 'spec/spec_helper.rb', '# config.mock_with :mocha', 'config.mock_with :mocha'
+inject_into_file 'spec/spec_helper.rb', "require 'capybara/rspec'\n", :after => "require 'rspec/rails'\n"
 
-generate 'devise:install'
-inject_into_file 'spec/spec_helper.rb', "  config.include Devise::TestHelpers, :type => :controller\n", :after => "RSpec.configure do |config|\n"
+apply File.join(File.dirname(__FILE__), 'defaults.rb')
+apply File.join(File.dirname(__FILE__), 'urgetopunt.rb')
+apply File.join(File.dirname(__FILE__), 'authlogic.rb') if @use_authlogic
+
 todo 'cancan', '$ rails g cancan:ability'
 
 generate 'jquery:install'
 gsub_file 'config/application.rb',
           'config.action_view.javascript_expansions[:defaults] = %w()',
-          'config.action_view.javascript_expansions[:defaults] = %w(jquery.min rails)'
+          'config.action_view.javascript_expansions[:defaults] = %w(jquery.min jquery_ujs)'
+
+remove_file 'app/views/layouts/application.html.erb'
+file 'app/views/layouts/application.html.haml', <<HAML
+!!! XML
+!!! 5
+%head
+  %title= yield :title
+  = stylesheet_link_tag :all
+  = csrf_meta_tag
+%body
+  #head
+    %h1= yield :title
+
+  #main
+    #flash
+      - flash.each do |level, message|
+        %p{:class => level}= message
+
+    = yield
+
+  #foot
+
+  = javascript_include_tag :defaults
+HAML
 
 generate 'formtastic:install'
 
-hoptoad_api_key = ask('What is the Hoptoad API key for this project (leave blank to skip)?')
-if hoptoad_api_key.present?
-  generate 'hoptoad', '--api-key', hoptoad_api_key
-  inject_into_file 'config/initializers/hoptoad.rb', "  config.js_notifier = true\n", :before => 'end'
+initializer 'rack_escape_utils.rb', <<RUBY
+# http://openhood.com/rack/ruby/2010/07/15/rack-test-warning/
+require 'escape_utils/html/rack'
+require 'escape_utils/html/haml'
+
+module Rack
+  module Utils
+    def escape(s)
+      EscapeUtils.escape_url(s)
+    end
+  end
+end
+RUBY
+
+if @require_ssl
+  prepend_file 'config/environments/production.rb', "require 'rack/ssl'\n\n"
+  inject_into_file 'config/environments/production.rb',
+    "  config.middleware.insert_before ActionDispatch::Cookies, Rack::SSL\n",
+    :after => "::Application.configure do\n"
+end
+
+if @hoptoad_api_key.present?
+  generate 'hoptoad', '--api-key', @hoptoad_api_key
+  inject_into_file 'app/views/layouts/application.html.haml', "= hoptoad_javascript_notifier\n  ", :before => "= csrf_meta_tag"
 else
   todo 'hoptoad', "$ rails g hoptoad --api-key HOPTOAD_API_KEY"
 end
+
+append_file '.gitignore', <<GIT
+*.swp
+*~
+#*#
+.DS_Store
+webrat.log
+GIT
+
+file '.autotest', "require 'autotest/bundler'\n"
