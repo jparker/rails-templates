37f0acbfd56f1e374710d04dd0967dfb69d4f29e
John Parker <jparker@urgetopunt.com>
2011-09-25 21:30:23 -0700

    formtastic 2.0

--- gems.rb
+++ gems.rb
@@ -1,24 +1,24 @@
-gem 'rspec-rails', version: '~>2.6.0', group: [:test, :development]
-gem 'factory_girl_rails', version: '~>1.2.0', group: :test
+gem 'rspec-rails', version: '~> 2.6.0', group: [:test, :development]
+gem 'factory_girl_rails', version: '~> 1.2.0', group: :test
 gem 'faker', group: [:test, :development]
-gem 'shoulda-matchers', version: '~>1.0.0.beta3', group: :test
-gem 'mocha', version: '~>0.9.11', group: :test
-gem 'capybara', group: :test, version: '~>1.1.0'
-gem 'launchy', version: '~>0.3.7', group: :test
+gem 'shoulda-matchers', version: '~> 1.0.0.beta3', group: :test
+gem 'mocha', version: '~> 0.9.11', group: :test
+gem 'capybara', group: :test, version: '~> 1.1.0'
+gem 'launchy', version: '~> 0.3.7', group: :test
 
 gem 'guard-rspec', group: :test
 gem 'growl_notify', group: :test
 gem 'rb-fsevent', group: :test, require: false
 
-gem 'kaminari', version: '~>0.12.3'
-gem 'formtastic', version: '~>2.0.0.rc5'
-gem 'inherited_resources', version: '~>1.2.1'
-gem 'responders', version: '~>0.6.4'
-gem 'haml', version: '~>3.1.1'
-gem 'validates_existence', version: '~>0.7.0'
+gem 'kaminari', version: '~> 0.12.3'
+gem 'formtastic', version: '~> 2.0.0'
+gem 'inherited_resources', version: '~> 1.2.1'
+gem 'responders', version: '~> 0.6.4'
+gem 'haml', version: '~> 3.1.1'
+gem 'validates_existence', version: '~> 0.7.0'
 gem 'sorcery' if use_sorcery?
-gem 'airbrake', version: '~>3.0'
-gem 'escape_utils', version: '~>0.2.3'
+gem 'airbrake', version: '~> 3.0'
+gem 'escape_utils', version: '~> 0.2.3'
 
 todo 'add newrelic_rpm to Gemfile when ready to configure', 'newrlic_rpm'
 
