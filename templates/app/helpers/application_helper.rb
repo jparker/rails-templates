6e60a83e9f7003729938ff747a678bb88d4a8621
John Parker <jparker@urgetopunt.com>
2012-01-02 15:28:01 -0800

    layouts using bootstrap

--- templates/app/helpers/application_helper.rb
+++ templates/app/helpers/application_helper.rb
@@ -5,6 +5,44 @@ module ApplicationHelper
     end
   end
 
+  def render_flash
+    flash.each do |level, message|
+      klass = level == :notice ? :success : level == :alert ? :error : level
+      content_tag :div, class: ['alert-message', klass], data: {alert: 'alert'} do
+        link_to('x', '#', class: 'close') << content_tag(:p, message)
+      end
+    end
+  end
+
+  def flag(text, level = nil)
+    content_tag :span, text, class: ['label', level].compact
+  end
+
+  def link_to_index(path, options = {})
+    text = options.delete(:text) || raw('&laquo; List')
+    link_to text, path, class: ['btn', 'small', 'primary'], title: 'List'
+  end
+
+  def link_to_show(path, options = {})
+    text = options.delete(:text) || raw('View &raquo;')
+    link_to text, path, class: ['btn', 'small', 'info'], title: 'View'
+  end
+
+  def link_to_new(path, options = {})
+    text = options.delete(:text) || raw('New &raquo;')
+    link_to text, path, class: ['btn', 'small', 'success'], title: 'New'
+  end
+
+  def link_to_edit(path, options = {})
+    text = options.delete(:text) || raw('Edit &rqauo;')
+    link_to text, path, class: ['btn', 'small'], title: 'Edit'
+  end
+
+  def link_to_destroy(path, options = {})
+    text = options.delete(:text) || raw('Delete &raquo;')
+    link_to text, path, class: ['btn', 'small', 'danger'], title: 'Delete', method: :delete, confirm: 'Are you sure?'
+  end
+
   def google_analytics(app_id)
     if Rails.env.production?
       javascript_tag <<-END
